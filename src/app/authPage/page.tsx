1e3d9c5da8f29cc8dadbfe41630acb50f21580da
Franco Armando <francoarmando1911@gmail.com>
2025-02-19 16:55:36 -0300

    actualizacion

---
+++ src/app/authPage/page.tsx
@@ -0,0 +1,14 @@
+//EN ESTA PAGINA DEBERIA DESARROLLAR TODAS LAS VISUALES DE LOGIN, REGISTER, ETC
+
+"use client"
+
+import React from 'react'
+
+export default function LoginPage() {
+  return (
+    <>
+    <h1 className='font-bold text-4xl text-black pt-10'>Pagina de autenticacion</h1>
+
+    </>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
+//Pagina principal, aca deberian ir importados los componentes globales de mi aplicacion
+
+"use client";
+
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen"> 
-      <h1 className="bg-red-700 text-4xl md:text-6xl font-semibold py-8 px-16 rounded-lg">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4 space-y-4 text-center">
+      <h1 className="bg-red-700 text-white text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-semibold py-4 sm:py-6 px-8 sm:px-12 rounded-lg shadow-lg">
         Elibel
       </h1>
+
+      <Link
+        href="/authPage"
+        className="bg-red-700 text-white text-base sm:text-lg font-medium py-2 px-4 rounded-md shadow-md transition duration-300 hover:bg-red-800"
+      >
+        Página de Auth
+      </Link>
     </div>
   );
 }
