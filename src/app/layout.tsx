b1fdead990a23dcf0b5fcbf6cc363bcee2212952
Franco Armando <francoarmando1911@gmail.com>
2025-02-19 16:42:42 -0300

    Implementacion de vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className="bg-slate-400">
         {children}
+        <Analytics/>
       </body>
     </html>
   );
