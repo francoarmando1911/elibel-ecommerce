bc80b70a0f4978faf082bfdd421d44f5c2083fa8
Franco Armando <francoarmando1911@gmail.com>
2025-02-21 16:47:50 -0300

    construccion de home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import HomePage from "@/components/HomePage/components/page";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col">
+    <div className="min-h-screen bg-[#B3A799] flex flex-col">
       <header className="w-full bg-white shadow-md fixed top-0 left-0 z-50">
         <Header />
       </header>
