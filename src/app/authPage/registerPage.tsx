62374ed6e57a10a88a6079dce06031465179855d
Franco Armando <francoarmando1911@gmail.com>
2025-02-24 17:25:28 -0300

    actualizaciones en el componente del login, faltan corregir errores del boton de register para que me redireccione a la url que yo necesito

---
+++ src/app/authPage/registerPage.tsx
@@ -0,0 +1,9 @@
+// pages/registerPage.tsx
+import React from 'react';
+import Register from '@/components/AuthPage/registerPage';
+
+const registerPage: React.FC = () => {
+    return <Register />;
+};
+
+export default registerPage;
